150608b07fb89015cf6c80ac8d6ebb01c714e762
Michael Park <mcypark@gmail.com>
2014-01-14 13:59:55 -0500

    thrower.test.cc: Added unit tests for THROW macro.

---
+++ thrower.test.cc
@@ -0,0 +1,16 @@
+#include <Base/thrower.h>
+
+#include <gtest/gtest.h>
+
+TEST(TThrower, Typical) {
+  EXPECT_THROW(
+      THROW(std::runtime_error) << "Some error message",
+      std::runtime_error);
+}
+
+TEST(TThrower, LogicError) {
+  EXPECT_THROW(
+      THROW(std::logic_error) << "Damn, logic error...",
+      std::logic_error);
+}
+
