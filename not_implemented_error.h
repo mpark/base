d2f007cbad19b719c1b5487d9f58853dd15fe244
Michael Park <mcypark@gmail.com>
2014-01-14 13:44:48 -0500

    not_implemented_error.h: An exception noting that a functionality is not implemented.

---
+++ not_implemented_error.h
@@ -0,0 +1,29 @@
+/* Defines a class to be thrown to denote that a functionality is not
+   implemented yet. */
+
+#pragma once
+
+#include <stdexcept>
+
+namespace Base {
+
+  /* Throw this exception when something isn't implemented yet.
+
+     For example,
+
+     Base::TThrower<Base::TNotImplementedError>(HERE)
+     << __func__ << " is not implemented.";
+     */
+  class TNotImplementedError : public std::runtime_error {
+    public:
+
+    /* Convenience type alias. */
+    using TSuper = std::runtime_error;
+
+    /* Inherit std::runtime_error's constructor. */
+    using TSuper::TSuper;
+
+  };  // TNotImplementedError
+
+}  // namespace Base
+
